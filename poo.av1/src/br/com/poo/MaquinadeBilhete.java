8174a3e64bc92dddcf26e86561f5e5fea49d6770
acfss <acf.antonio@gmail.com>
2021-05-07 17:25:56 -0300

    adicionei novos locais

--- poo.av1/src/br/com/poo/MaquinadeBilhete.java
+++ poo.av1/src/br/com/poo/MaquinadeBilhete.java
@@ -9,10 +9,13 @@ public class MaquinadeBilhete {
 	public static void main(String[] args) {
 		Evento evento = new Evento();
 		Local local = new Local ("Arena", 20);
+		Local local1 = new Local ("Concha Acustica", 50);
+		Local local2 = new Local ("Went Wild", 30);
 		
-		local.addLocal("TCA", 100);	
-		evento.addEvento("Festival de Rock", 10, 6, 2022, "Paralamas", new Local("Concha Acustica", 20), 50);
-		evento.addEvento("Festival de Inverno", 15, 9, 2022, "Caetano Veloso", new Local("Went Wild", 30), 150);		
+		
+		
+		evento.addEvento("Festival de Rock", 10, 6, 2022, "Paralamas", local1, 50);
+		evento.addEvento("Festival de Inverno", 15, 9, 2022, "Caetano Veloso", local2, 150);		
 		evento.addEvento("Festival do Outono", 20, 10, 2022, "Harmonia do Samba", local, 70);
 		
 		
@@ -21,7 +24,7 @@ public class MaquinadeBilhete {
 		
 		evento.comprarIngresso("Festival de Inverno", 30);
 		evento.ingressoDisp("Festival de Inverno");
-		evento.consultEventodisp();
+		//evento.consultEventodisp();
 		//evento.comprarIngresso("Festival de Inverno", 15);
 		//evento.ingressoDisp("Festival de Inverno");
 		//evento.comprarIngresso("Festival de Inverno", 10);
