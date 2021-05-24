fe2a490aa1ef87bc9df72f35df9c57eadf41954a
acfss <acf.antonio@gmail.com>
2021-05-24 17:39:19 -0300

    att

--- poo.at5/src/br/com/poo/AppMain.java
+++ poo.at5/src/br/com/poo/AppMain.java
@@ -5,7 +5,6 @@ import br.com.poo.modelo.*;
 public class AppMain {
 
 	public static void main(String[] args) {
-		//Contribuinte.listaContr()[3].calcImposto();
 		Contribuinte.listaContr()[1].calcImposto();
 		Contribuinte.listaContr()[3].calcImposto();
 		Contribuinte.listaContr()[5].calcImposto();
